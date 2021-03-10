e88729b75c46ab3fa2dabfdfe95ae4a0f75012a9
CorineNijhof <c.nijhof@student.rug.nl>
2021-03-10 11:42:22 +0100

    read data into tensors

---
+++ launch.py
@@ -0,0 +1,4 @@
+from readData import get_data
+
+train_images, train_labels = get_data('fashion-mnist_train.csv')
+test_images, test_labels = get_data('fashion-mnist_test.csv')
---
+++ readData.py
@@ -0,0 +1,32 @@
+import zipfile
+import numpy as np
+import torch
+
+
+# load data and labels from zipfile, and store in numpy arrays
+def load_data(path):
+    with zipfile.ZipFile('data.zip') as data:
+        data_file = data.open(path)
+    images = []
+    labels = []
+    for idx, line in enumerate(data_file):
+        if idx == 0:
+            continue
+        line = str(line)[:-3]
+        image = line.split(',')
+        labels.append(int(image[0][2:]))
+        image = list(map(int, image[1:]))
+        images.append(image)
+    return np.array(images).reshape(len(images), 1, 28, 28), np.array(labels)
+
+
+# load data, change data to floats and get data to gpu (if cuda available)
+def get_data(path):
+    data, labels = load_data(path)
+    data = torch.from_numpy(data)
+    labels = torch.from_numpy(labels)
+    data = data.float()
+    if torch.cuda.is_available():
+        data = data.cuda()
+        labels = labels.cuda()
+    return data, labels
