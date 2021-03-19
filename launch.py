c49842e76d0973029c3754acec24ce3df4b47f03
CorineNijhof <c.nijhof@student.rug.nl>
2021-03-19 15:58:22 +0100

    added data shuffle with fixed random seed

--- launch.py
+++ launch.py
@@ -3,11 +3,13 @@ import matplotlib.pyplot as plt
 
 
 # just to show an image to show it is working, can be deleted at some point
-def show_image(data, labels, batch, idx):
-    print("label = ", labels[batch][idx].item())
-    plt.imshow(data[batch][idx].cpu().permute(1, 2, 0))
+def show_image(data_, labels_, batch, idx):
+    print("label = ", labels_[batch][idx].item())
+    plt.imshow(data_[batch][idx].cpu().permute(1, 2, 0))
     plt.show()
 
+
 if __name__ == '__main__':
-    data, labels = load_data('data') # all data, divided into batches
+    data, labels = load_data('data')  # all data, divided into batches
     # show_image(data, labels, 0, 0)
+    print('data loaded')
--- readData.py
+++ readData.py
@@ -2,16 +2,21 @@ import torch
 from torch.utils.data import DataLoader
 from torchvision import transforms, datasets
 
+
 # load the image data and labels into lists of tensors
 def load_data(path):
-    transformer = transforms.Compose([transforms.Resize((255, 255)), transforms.ToTensor()]) # resize image, 255 chosen at random atm
-    dataset = datasets.ImageFolder(path, transform = transformer)
-    dataloader = DataLoader(dataset, batch_size = 250, num_workers = 8, pin_memory=True) # num_worker for parallel, pin_memory should improve speed lateron
+    torch.manual_seed(1)
+    # resize image, 255 chosen at random atm
+    transformer = transforms.Compose([transforms.Resize((255, 255)), transforms.ToTensor()])
+    dataset = datasets.ImageFolder(path, transform=transformer)
+    # num_worker for parallel, pin_memory should improve speed later on
+    dataloader = DataLoader(dataset, batch_size=250, num_workers=6, pin_memory=True, shuffle=True)
 
     data = []
     labels = []
     for image_batch, label_batch in dataloader:
-        if torch.cuda.is_available():               # transfer data to gpu
+        if torch.cuda.is_available():
+            # transfer data to gpu
             image_batch = image_batch.cuda()
             label_batch = label_batch.cuda()
         data.append(image_batch)
