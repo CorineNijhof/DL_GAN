295a77355fb6244f49a4c6d2d8920a06504cb48d
HiddevdBos <hiddevandenbos@upcmail.nl>
2021-03-25 16:57:24 +0100

    start generator

--- readData.py
+++ readData.py
@@ -7,18 +7,19 @@ from torchvision import transforms, datasets
 def load_data(path):
     torch.manual_seed(1)
     # resize image, 255 chosen at random atm
-    transformer = transforms.Compose([transforms.Resize((255, 255)), transforms.ToTensor()])
+    transformer = transforms.Compose([transforms.Resize((64, 64)), transforms.ToTensor()])
     dataset = datasets.ImageFolder(path, transform=transformer)
     # num_worker for parallel, pin_memory should improve speed later on
-    dataloader = DataLoader(dataset, batch_size=250, num_workers=6, pin_memory=True, shuffle=True)
+    dataloader = DataLoader(dataset, batch_size=250, num_workers=8, pin_memory=True, shuffle=True)
 
     data = []
     labels = []
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     for image_batch, label_batch in dataloader:
-        if torch.cuda.is_available():
-            # transfer data to gpu
-            image_batch = image_batch.cuda()
-            label_batch = label_batch.cuda()
+        # transfer data to gpu
+        image_batch = image_batch.cuda(device)
+        label_batch = label_batch.to(device)
+        
         data.append(image_batch)
         labels.append(label_batch)
     
