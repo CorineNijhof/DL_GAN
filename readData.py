626a3e27afbef65c8273c94d2e78d5b4077a8662
CorineNijhof <c.nijhof@student.rug.nl>
2021-03-26 10:42:44 +0100

    reformat

--- readData.py
+++ readData.py
@@ -4,13 +4,13 @@ from torchvision import transforms, datasets
 
 
 # load the image data and labels into lists of tensors
-def load_data(path):
+def load_data(path, batch_size=135):
     torch.manual_seed(1)
     # resize image, 255 chosen at random atm
     transformer = transforms.Compose([transforms.Resize((64, 64)), transforms.ToTensor()])
     dataset = datasets.ImageFolder(path, transform=transformer)
     # num_worker for parallel, pin_memory should improve speed later on
-    dataloader = DataLoader(dataset, batch_size=250, num_workers=8, pin_memory=True, shuffle=True)
+    dataloader = DataLoader(dataset, batch_size=batch_size, num_workers=8, pin_memory=True, shuffle=True)
 
     data = []
     labels = []
@@ -19,7 +19,7 @@ def load_data(path):
         # transfer data to gpu
         image_batch = image_batch.cuda(device)
         label_batch = label_batch.to(device)
-        
+
         data.append(image_batch)
         labels.append(label_batch)
     
--- run_generator.py
+++ run_generator.py
@@ -9,42 +9,43 @@ def weights_init(m):
     classname = m.__class__.__name__
     print(classname)
     if classname.find('Conv') != -1:
-    	nn.init.normal_(m.weight.data, 0.0, 0.02)
+        nn.init.normal_(m.weight.data, 0.0, 0.02)
     elif classname.find('BatchNorm') != -1:
-    	nn.init.normal_(m.weight.data, 1.0, 0.02)
-    	nn.init.constant_(m.bias.data, 0)
+        nn.init.normal_(m.weight.data, 1.0, 0.02)
+        nn.init.constant_(m.bias.data, 0)
 
 
 def show_image(generator, fixed_noise):
-	generated_img = generator(fixed_noise).cpu().detach()
+    generated_img = generator(fixed_noise).cpu().detach()
 
-	plt.imshow(generated_img.squeeze().permute(1,2,0))
-	plt.show()
+    plt.imshow(generated_img.squeeze().permute(1, 2, 0))
+    plt.show()
+
 
 def run_generator():
-	# torch.cuda.is_available = lambda : False
+    # torch.cuda.is_available = lambda : False
 
-	device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-	print(device)
-	generator = Generator().to(device)
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print(device)
+    generator = Generator().to(device)
 
-	# Handle multi-gpu if desired
-	# if (device.type == 'cuda') and (ngpu > 1):
-	    # netG = nn.DataParallel(netG, list(range(ngpu)))
+    # Handle multi-gpu if desired
+    # if (device.type == 'cuda') and (ngpu > 1):
+    # netG = nn.DataParallel(netG, list(range(ngpu)))
 
-	# Apply the weights_init function to randomly initialize all weights
-	#  to mean=0, stdev=0.2.
-	fixed_noise = torch.randn(1, 64, 1, 1, device=device)
-	# fixed_noise = torch.randn(64, 64, 1, 1, device=device)
+    # Apply the weights_init function to randomly initialize all weights
+    #  to mean=0, stdev=0.2.
+    fixed_noise = torch.randn(1, 64, 1, 1, device=device)
+    # fixed_noise = torch.randn(64, 64, 1, 1, device=device)
 
-	optimizer_generator = Adam(generator.parameters())
-	criterion = nn.BCELoss()
-	generator.apply(weights_init)
+    optimizer_generator = Adam(generator.parameters())
+    criterion = nn.BCELoss()
+    generator.apply(weights_init)
 
-	# Print the model
-	print(generator)
+    # Print the model
+    print(generator)
 
-	show_image(generator, fixed_noise)
+    show_image(generator, fixed_noise)
 
 
 run_generator()
