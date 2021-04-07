bcae0d15a42b014c06597cdf4a1f18f14899cd94
CorineNijhof <c.nijhof@student.rug.nl>
2021-04-07 17:14:54 +0200

    update peregrine_bash

--- peregrine_bash.sh
+++ peregrine_bash.sh
@@ -10,4 +10,9 @@ module load Python/3.8.6-GCCcore-10.2.0
 # source /data/s3219496/.envs/gan_env/bin/activate
 source /data/s2967383/.envs/gan_env/bin/activate
 
-python3 launch.py
+#python3 launch.py test VANGAN
+#python3 launch.py test default
+python3 launch.py paintings VANGAN
+#python3 launch.py paintings default
+#python3 launch.py paris VANGAN
+#python3 launch.py paris default
