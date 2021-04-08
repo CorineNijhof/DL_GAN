a23a619f70e2adbc7d80275559f63e3dd4743c6f
CorineNijhof <c.nijhof@student.rug.nl>
2021-04-08 11:04:46 +0200

    fix learning rate input string error

--- launch.py
+++ launch.py
@@ -21,7 +21,7 @@ if __name__ == '__main__':
     if len(sys.argv) > 2:
         net = sys.argv[2]
     if len(sys.argv) > 3:
-        lr = int(sys.argv[3])
+        lr = float(sys.argv[3])
     if len(sys.argv) > 4:
         optimizerD = sys.argv[4]
 
@@ -42,7 +42,7 @@ if __name__ == '__main__':
 
     # load the data in batches
     batch_size = 64
-    dataloader, num_images = load_data(path=path, batch_size=batch_size, num_workers=6)
+    dataloader, num_images = load_data(path=path, batch_size=batch_size, num_workers=8)
     print('data loaded')
 
     train(dataloader, num_epochs=num_epochs, net=net, learning_rate=lr, optimizerD=optimizerD,
