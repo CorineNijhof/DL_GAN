286af0512fecddb0146d50501798fbe464d66f20
CorineNijhof <c.nijhof@student.rug.nl>
2021-04-08 19:51:28 +0200

    update peregrine_bash

--- peregrine_bash.sh
+++ peregrine_bash.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --time=02:30:00
 #SBATCH --partition=gpu
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:v100:1
 
 module load libpciaccess/0.16-GCCcore-9.3.0
 module load Python/3.8.6-GCCcore-10.2.0
@@ -12,9 +12,9 @@ source /data/s2967383/.envs/gan_env/bin/activate
 
 #python3 launch.py test default
 #python3 launch.py test VANGAN
-#python3 launch.py data default 0.0002 Adam
-#python3 launch.py drawings default 0.0002 Adam
-python3 launch.py paris default 0.0002 Adam
+python3 launch.py data default 0.0002 Adam
+python3 launch.py drawings default 0.0002 Adam
+#python3 launch.py paris default 0.0002 Adam
 #python3 launch.py paintings default 0.0002 Adam
 #python3 launch.py paintings VANGAN 0.0002 Adam
 #python3 launch.py paintings default 0.01 Adam
